1f4edee95c55ab2fac3c014f0b7bdcd78be5b5fd
Yohannesmoke <yohannesmekonen091355@gmail.com>
2025-07-29 09:47:45 +0300

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <section className="py-20">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+            <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
+              <h1 className="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
+                Hub Technologies
+                <span className="block text-orange-500">Your Number 1 choice when it comes to Technology</span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
+                Launch your SaaS product in record time with our powerful,
+                ready-to-use template. Packed with modern technologies and
+                essential integrations.
+              </p>
+              <div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
+                <a
+                  href="https://vercel.com/templates/next.js/next-js-saas-starter"
+                  target="_blank"
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
